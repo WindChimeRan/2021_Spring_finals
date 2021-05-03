538748770c248dded9a6da66ede1393aa221f8ba
WindChimeRan <1012869439@qq.com>
2021-05-03 01:41:00 -0500

    demo/simulaiton

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # 2021_Spring_finals
 
-All final assignments (projects) for IS597PR in Spring 2021 are expected to be forks from here.
-
-See the course Moodle for the detailed expectations and instructions.
+```sh
+python main.py --demo
+python main.py --simulation
+```
--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-"""This example spawns (bouncing) balls randomly on a L-shape constructed of 
-two segment shapes. Not interactive.
-"""
-
-__version__ = "$Id:$"
-__docformat__ = "reStructuredText"
-
-# Python imports
-import random
+import numpy as np
 from typing import List
-
-# Library imports
+from pymunk.vec2d import Vec2d
 import pygame
+import argparse
 
-# pymunk imports
 import pymunk
 import pymunk.pygame_util
 from pymunk.vec2d import Vec2d
 
-class Bread(object):
 
-    def __init__(self, height) -> None:
+class Bread(object):
+    def __init__(self, height, delta) -> None:
         x = 600
-        delta = 30
+        delta = 0
         # x, y = position
         y = height
         x -= delta
@@ -37,6 +28,7 @@ class Bread(object):
         self.body = body
         self.shape = shape
 
+
 class Surface(object):
     def __init__(self, height) -> None:
         x = 600
@@ -47,7 +39,7 @@ class Surface(object):
         # moment = pymunk.moment_for_poly(mass, fp)
 
         r_flipper_body = pymunk.Body()
-        r_flipper_body.position = x + 120, y + 20 - 3
+        r_flipper_body.position = x + 120, y + 10
         r_flipper_shape = pymunk.Poly(r_flipper_body, fp)
         r_flipper_shape.mass = mass
         r_flipper_shape.friction = 0.9
@@ -60,11 +52,11 @@ class Surface(object):
             r_flipper_body, r_flipper_joint_body, 0.15, 20000000, 900000
         )
 
-
         self.body = r_flipper_body
         self.shape = r_flipper_shape
         self.j = j
         self.s = s
+
 
 class Environment(object):
     """
@@ -72,68 +64,84 @@ class Environment(object):
     that don't move. Balls appear occasionally and drop onto the platform. They bounce around.
     """
 
-    def __init__(self, bread, surface) -> None:
+    def __init__(self, bread, surface, impulse, display=False) -> None:
         # Space
+        self.display = display
         self._space = pymunk.Space()
         self._space.gravity = (0.0, 4 * 980)
 
         # Physics
         # Time step
-        self._dt = 1.0 / 60.0
+        self._dt = 5.0 / 600.0
         # Number of physics steps per screen frame
         self._physics_steps_per_frame = 1
 
         # pygame
         self.width = 1000
-        self.height = 1200 
-        pygame.init()
-        self._screen = pygame.display.set_mode((self.width, self.height))
-        self._clock = pygame.time.Clock()
+        self.height = 1200
 
-        self._draw_options = pymunk.pygame_util.DrawOptions(self._screen)
+        if display:
+            pygame.init()
+            self._screen = pygame.display.set_mode((self.width, self.height))
+            self._clock = pygame.time.Clock()
 
-        # Static barrier walls (lines) that the balls bounce off of
-        self._add_static_scenery()
+            self._draw_options = pymunk.pygame_util.DrawOptions(self._screen)
+
+        self._add_ground()
 
         self.bread = bread
         self.surface = surface
-
+        self.impulse = impulse
 
         # Execution control and time until the next ball spawns
         self._running = True
         self._ticks_to_next_ball = 10
 
-    def run(self) -> None:
+    def run(self) -> bool:
         """
         The main loop of the game.
         :return: None
         """
-        self._space.add(bread.body, bread.shape)
-        self._space.add(surface.body, surface.shape)
-        self._space.add(surface.j, surface.s)
-        # surface.body.apply_impulse_at_local_point(
-        #     Vec2d.unit() * -6000, (-100, 0)
-        # )
+        self._space.add(self.bread.body, self.bread.shape)
+        self._space.add(self.surface.body, self.surface.shape)
+        self._space.add(self.surface.j, self.surface.s)
+
+        self._space.step(0.1)
+        self.surface.body.apply_impulse_at_local_point(
+            Vec2d.unit() * self.impulse, (-50, 0)
+        )
         # Main loop
-        # self._create_bread()
         while self._running:
             # Progress time forward
             for x in range(self._physics_steps_per_frame):
                 self._space.step(self._dt)
-            print(self.bread.body._get_angle() / (3.14/2))
-            self._process_events()
-            # self._create_bread()
-            # self._update_balls()
-            self._clear_screen()
-            self._draw_objects()
-            pygame.display.flip()
-            # Delay fixed time between frames
-            self._clock.tick(500)
-            pygame.display.set_caption("fps: " + str(self._clock.get_fps()))
+            half_turn = self.bread.body._get_angle() / (3.14 / 2)
+            butter_side_down = bool(round(half_turn) % 2)
 
-    def _add_static_scenery(self) -> None:
+            _, by = self.bread.body.position
+            gy = 900
+            dis = gy - by
+
+            if 23.86 < abs(dis) < 24:
+                if self.display:
+                    self._clock.tick(1)
+                # print(half_turn)
+                break
+
+            if self.display:
+                pygame.display.flip()
+                # Delay fixed time between frames
+                self._clock.tick(10)
+
+                self._process_events()
+                self._clear_screen()
+                self._draw_objects()
+                pygame.display.set_caption("degree = %.1f" % (half_turn * 180))
+        return butter_side_down
+
+    def _add_ground(self) -> None:
         """
-        Create the static bodies.
+        Create ground.
         :return: None
         """
         static_body = self._space.static_body
@@ -143,9 +151,9 @@ class Environment(object):
         ground.elasticity = 0.9
         ground.friction = 0.9
 
+        self.ground = ground
+
         self._space.add(ground)
-
-
 
     def _process_events(self) -> None:
         """
@@ -157,40 +165,6 @@ class Environment(object):
                 self._running = False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 self._running = False
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-                pygame.image.save(self._screen, "bouncing_balls.png")
-
-    # def _update_balls(self) -> None:
-    #     """
-    #     Create/remove balls as necessary. Call once per frame only.
-    #     :return: None
-    #     """
-    #     self._ticks_to_next_ball -= 1
-    #     if self._ticks_to_next_ball <= 0:
-    #         self._create_bread()
-    #         self._ticks_to_next_ball = 100
-    #     # Remove balls that fall below 100 vertically
-    #     balls_to_remove = [ball for ball in self._rods if ball.body.position.y > 700]
-    #     for ball in balls_to_remove:
-    #         self._space.remove(ball, ball.body)
-    #         self._rods.remove(ball)
-
-    # def _create_bread(self) -> None:
-    #     """
-    #     Create a rods (bread).
-    #     :return:
-    #     """
-    #     mass = 10
-    #     inertia = pymunk.moment_for_segment(mass, (1, 2), (4, 2), 2)
-    #     body = pymunk.Body(mass, inertia)
-
-    #     x = 200
-    #     y = 400 - 300
-    #     body.position = x, y
-    #     shape = pymunk.Segment(body, (0, 40), (40, -40), 6)
-    #     shape.elasticity = 0.8
-    #     shape.friction = 0.9
-    #     self._space.add(body, shape)
 
     def _clear_screen(self) -> None:
         """
@@ -207,11 +181,49 @@ class Environment(object):
         self._space.debug_draw(self._draw_options)
 
 
+def simulation():
+    # print('sim')
+    height = np.random.random(1000) * 130 + 50 # 50 - 180
+    # height = np.random.random(1000) * 200  # 50 - 180
+
+    height = 890 - height * 4
+
+    # Palm force: n(0, 10000)
+    impluse = np.random.normal(0, 1000, size=1000)
+    # relative position between the bread and the palm: u(-15, 0).
+    delta = -np.random.random(1000) * 60
+
+    results = []
+    for h, i, d in zip(height, impluse, delta):
+        bread = Bread(h, d)
+        surface = Surface(h)
+        env = Environment(bread, surface, i, display=False)
+        butter_side_down = env.run()
+        results.append(butter_side_down)
+    # print(results)
+    print(
+        "probability of butter-side down is %.1f%%"
+        % (sum(results) * 100 / len(results))
+    )
+
+
+def demo():
+    h = 175
+    height = 890 - h * 4
+    bread = Bread(height, 0)
+    surface = Surface(height)
+    env = Environment(bread, surface, 10000, display=True)
+    env.run()
+
+
 if __name__ == "__main__":
     # position = 600, 320
-    h = 75
-    height = 890 - h * 4
-    bread = Bread(height)
-    surface = Surface(height)
-    env = Environment(bread, surface)
-    env.run()
+    # simulation()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--demo", action="store_true")
+    parser.add_argument("--simulation", action="store_true")
+    args = parser.parse_args()
+    if args.simulation:
+        simulation()
+    if args.demo:
+        demo()
